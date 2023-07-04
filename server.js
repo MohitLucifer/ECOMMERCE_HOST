8fe1d9a3f859fa228fc79968dabeac98f821d30b
MOHIT KUMAR <100012404+MohitLucifer@users.noreply.github.com>
2023-07-04 20:45:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,13 +6,18 @@ import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
 import cors from "cors";
 import categoryRoutes from "./routes/categoryRoutes.js"
-import  productRoutes from './routes/productRoutes.js'
-import path from 'path'
+import  productRoutes from "./routes/productRoutes.js";
+import path from 'path';
+import {fileURLToPath} from 'url';
 //configure env
 dotenv.config();
 
 //databse config
 connectDB();
+
+//esmodule fix 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //rest object
 const app = express();
